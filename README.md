d19415ac17269a10b8a6bc403c6600efe30a23d9
SkybuckFlying <skybuck2000@hotmail.com>
2017-12-25 16:11:38 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Hello World,
+
+This is Skybuck Flyin's GitHub testing repository ! =D
+
+Bye World ! =D,
+  Skybuck Flying ! =D
+  
---
+++ TestProgram.dpr
@@ -0,0 +1,38 @@
+program TestProgram;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils;
+
+procedure Main;
+begin
+	writeln('program started');
+
+	// ... SomeCode1 ...
+	// ... SomeCode2 ...
+	// ... SomeCode3 ...
+
+	// ... SomeCode4 ...
+	// ... SomeCode5 ...
+	// ... SomeCode6 ...
+
+	// ... SomeCode7 ...
+	// ... SomeCode8 ...
+	// ... SomeCode9 ...
+
+	writeln('program finished');
+end;
+
+
+begin
+	try
+		Main;
+	except
+		on E: Exception do
+			Writeln(E.ClassName, ': ', E.Message);
+	end;
+	ReadLn;
+end.
